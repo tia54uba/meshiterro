21f8b9954a40cad0b5c70f9111a0fcdd7846f1fd
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-01-13 11:07:09 +0000

    [Finish]4-11

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :post_images, only: [:new, :create, :index, :show]
+  #get 'post_image/new'
+  #get 'post_image/index'
+  #get 'post_image/show'
   devise_for :users
 root to: "homes#top"
   get 'homes/about' ,as: 'about'
