d4b3edb68f6389cb01dfe34cd5ec86eb83857581
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-01-15 05:08:57 +0000

    [Finish]4-13

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   #get 'post_image/new'
   #get 'post_image/index'
   #get 'post_image/show'
